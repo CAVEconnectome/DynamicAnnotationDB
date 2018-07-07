b7a6f4c468a935ee2f2b589f5b465e1b29ee30cc
sdorkenw <sven.dorkenwald@googlemail.com>
2018-07-07 00:31:08 -0700

    generated setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="DynamicAnnotationDB",
+    version="0.1",
+    author="Sven Dorkenwald",
+    author_email="",
+    description="Annotation Database pendant to the chunkedgraph",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/seung-lab/DynamicAnnotationDB",
+    packages=find_packages(),
+    install_requires=[
+        "google-cloud-bigtable"
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ),
+)
