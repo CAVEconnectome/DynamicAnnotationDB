9f2ed16efe5021054d4457538fb32b861d5ad263
Forrest Collman <forrest.collman@gmail.com>
2020-06-18 17:05:53 -0700

    removed dataset_name from Metadata table

--- dynamicannotationdb/models.py
+++ dynamicannotationdb/models.py
@@ -5,13 +5,11 @@ class Metadata(Base):
     __tablename__ = 'annotation_table_metadata'
     id = Column(Integer, primary_key=True)
     schema_type = Column(String(100), nullable=False)
-    table_name = Column(String(100), nullable=False)
-    dataset_name = Column(String(100), nullable=False)
+    table_name = Column(String(100), nullable=False, unique=True)
     valid = Column(Boolean)
     created = Column(DateTime, nullable=False)
     deleted = Column(DateTime, nullable=True)
     user_id = Column(String(255), nullable=False)
     description = Column(Text, nullable=False)
     reference_table = Column(String(100), nullable=True)
-    __table_args__ = (UniqueConstraint('table_name', 'dataset_name', name='_dataset_table_uc'),)
 
