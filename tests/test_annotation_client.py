c1e87f896044469d7f7e3415a4ff7769e30e9c61
dlbrittain <dlbrittain@gmail.com>
2020-06-29 11:56:55 -0700

    add empty tests

---
+++ tests/test_annotation_client.py
@@ -0,0 +1,33 @@
+import pytest
+
+
+def test_get_existing_tables_metadata():
+    assert False
+
+
+def test_create_table():
+    assert False
+
+
+def test_delete_table():
+    assert False
+
+
+def test_drop_table():
+    assert False
+
+
+def test_insert_annotation():
+    assert False
+
+
+def test_get_annotation():
+    assert False
+
+
+def test_update_annotation():
+    assert False
+
+
+def test_delete_annotation():
+    assert False
---
+++ tests/test_interface.py
@@ -0,0 +1,49 @@
+import pytest
+
+
+def test_create_annotation_table():
+    assert False
+
+
+def test_create_segmentation_table():
+    assert False
+
+
+def test_get_table_metadata():
+    assert False
+
+
+def test_get_table_schema():
+    assert False
+
+
+def test_get_table_sql_metadata():
+    assert False
+
+
+def test_get_model_columns():
+    assert False
+
+
+def test_get_existing_tables():
+    assert False
+
+
+def test_has_table():
+    assert False
+
+
+def test_cached_table():
+    assert False
+
+
+def test_get_table_row_count():
+    assert False
+
+
+def test_get_annotation_table_size():
+    assert False
+
+
+def test__drop_table():
+    assert False
