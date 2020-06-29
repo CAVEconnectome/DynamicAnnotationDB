3857e2ae28e38a85f9b3cdee9579b3136e0d5183
dlbrittain <dlbrittain@gmail.com>
2020-06-29 11:53:14 -0700

    Add addtional custom errors

--- dynamicannotationdb/errors.py
+++ dynamicannotationdb/errors.py
@@ -1,2 +1,17 @@
-class TableNameNotFoundException(KeyError):
-    ''' Table name is not found in Metadata table '''
+class TableNameNotFound(KeyError):
+    """ Table name is not found in Metadata table """
+
+class BadRequest(Exception):
+    pass
+
+class AnnotationInsertLimitExceeded(ValueError):
+    """Exception raised when amount of annotations exceeds defined limit.
+    """
+
+    def __init__(self, limit: int, length: int, message: str ="Annotation limit exceeded"):
+        self.limit = limit
+        self.message = f"The insertion limit is {limit}, {length} were attempted to be inserted"
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"{self.limit} -> {self.message}"
