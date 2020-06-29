ab4ad7f4fee03e59a80c90516ea263a1ef683cd2
dlbrittain <dlbrittain@gmail.com>
2020-06-29 11:56:16 -0700

    update example script

--- scripts/example_insert.py
+++ scripts/example_insert.py
@@ -1,15 +1,14 @@
 from dynamicannotationdb.annotation_client import DynamicAnnotationClient
 from dynamicannotationdb.key_utils import build_table_id
-# Test URI
-SQL_URI = "postgres://postgres:annodb@localhost:5432/annodb" 
 
 # Create annotation db instance
+aligned_volume = 'foo'
+sql_base_uri = "postgres://postgres:annodb@localhost:5432/"
 
-
-aligned_volume = 'minnie'
+# aligned_volume = 'minnie'
 table_name = 'synapse_test'
 schema_name = 'synapse'
-client = DynamicAnnotationClient(aligned_volume=aligned_volume, sql_base_uri=SQL_URI)
+client = DynamicAnnotationClient(aligned_volume=aligned_volume, sql_base_uri=sql_base_uri)
 
 test_table_description = "This is an example description for this table"
 
@@ -25,7 +24,7 @@ tables = client.get_existing_tables()
 # Lets add a synapse
 
 synapse_data = {
-    'id': 65534332,
+    'id': 1,
     'pre_pt': {'position': [121,123,1232], 'supervoxel_id':  2344444, 'root_id': 4},
     'ctr_pt': {'position': [121,123,1232]},
     'post_pt':{'position': [333,555,5555], 'supervoxel_id':  3242424, 'root_id': 5},
@@ -37,13 +36,14 @@ synapse_data = {
 insert new data, if assign_id is True, the Database PK will be overriden by the 'id' in
 synapse_data
 """
-client.insert_annotations(table_name, schema_name, [synapse_data])
+client.insert_annotations(table_name, [synapse_data])
 # Get the newly inserted row
-anns=client.get_annotations(table_name, schema_name, [1])
+anns=client.get_annotations(table_name, [1])
+print(anns)
 
-
+# update a specific row by id
 updated_synapse_data = {
-    'id': 65534332,
+    'id': 1,
     'pre_pt': {'position': [121,123,1232], 'supervoxel_id':  2344444, 'root_id': 4},
     'ctr_pt': {'position': [121,123,1232]},
     'post_pt':{'position': [555,555,5555], 'supervoxel_id':  3242424, 'root_id': 5},
@@ -51,4 +51,16 @@ updated_synapse_data = {
 }  
 
 # Update annotation
-client.update_annotation(table_name, schema_name, 1, updated_synapse_data)
+client.update_annotation(table_name, updated_synapse_data)
+
+# data to update same row again
+updated_synapse_data = {
+    'id': 1,
+    'pre_pt': {'position': [121,123,1232], 'supervoxel_id':  2344444, 'root_id': 4},
+    'ctr_pt': {'position': [121,123,1232]},
+    'post_pt':{'position': [555,555,5555], 'supervoxel_id':  3242424, 'root_id': 5},
+    'type': 'synapse',
+}  
+
+# Try to over write a depreciated annotation returns an error
+client.update_annotation(table_name, updated_synapse_data)
