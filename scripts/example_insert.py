4846a30e23248044d6e50a236243213cc43f1c64
dlbrittain <dlbrittain@gmail.com>
2020-07-03 11:34:42 -0700

    update simple example script

--- scripts/example_insert.py
+++ scripts/example_insert.py
@@ -5,7 +5,6 @@ from dynamicannotationdb.key_utils import build_table_id
 aligned_volume = 'foo'
 sql_base_uri = "postgres://postgres:annodb@localhost:5432/"
 
-# aligned_volume = 'minnie'
 table_name = 'synapse_test'
 schema_name = 'synapse'
 client = DynamicAnnotationClient(aligned_volume=aligned_volume, sql_base_uri=sql_base_uri)
@@ -14,17 +13,17 @@ test_table_description = "This is an example description for this table"
 
 new_table = client.create_table(table_name, 
                                 schema_name,
-                                metadata_dict={
-                                    'description':test_table_description,
-                                    'user_id': 'foo@bar.com'})
+                                description='test_table_description',
+                                user_id='foo@bar.com')
 
 # get list of tables in aligned volume
-tables = client.get_existing_tables()
+
+tables = client._get_existing_table_ids()
+
 
 # Lets add a synapse
 
 synapse_data = {
-    'id': 1,
     'pre_pt': {'position': [121,123,1232], 'supervoxel_id':  2344444, 'root_id': 4},
     'ctr_pt': {'position': [121,123,1232]},
     'post_pt':{'position': [333,555,5555], 'supervoxel_id':  3242424, 'root_id': 5},
@@ -39,7 +38,7 @@ synapse_data
 client.insert_annotations(table_name, [synapse_data])
 # Get the newly inserted row
 anns=client.get_annotations(table_name, [1])
-print(anns)
+
 
 # update a specific row by id
 updated_synapse_data = {
