75557249a591af956d78f16ff0536975e36c964b
dlbrittain <dlbrittain@gmail.com>
2020-07-03 11:31:36 -0700

    Add UpdateAnnotation error

--- dynamicannotationdb/errors.py
+++ dynamicannotationdb/errors.py
@@ -7,6 +7,18 @@ class TableAlreadyExists(KeyError):
 class BadRequest(Exception):
     pass
 
+
+class UpdateAnnotationError(ValueError):
+    
+    def __init__(self, target_id: int, superceded_id: int, message: str ="Annotation limit exceeded"):
+        self.target_id = target_id
+        self.message = f"""Annotation with ID {target_id} has already been superseded by annotation ID {superceded_id},
+                        update annotation ID {superceded_id} instead"""
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"Error update ID {self.target_id} -> {self.message}"
+
 class AnnotationInsertLimitExceeded(ValueError):
     """Exception raised when amount of annotations exceeds defined limit.
     """
