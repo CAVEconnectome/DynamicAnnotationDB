c9806c7031daa9ae8e2a1b4acbd31153978164fc
dlbrittain <dlbrittain@gmail.com>
2020-07-20 12:42:39 -0700

    add segmentation metadata model

--- dynamicannotationdb/models.py
+++ dynamicannotationdb/models.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, Boolean, String, UniqueConstraint, Integer, DateTime, Text
+from sqlalchemy import Column, Boolean, String, UniqueConstraint, Integer, DateTime, Text, ForeignKey
 from emannotationschemas.models import Base
 
-class Metadata(Base):
+class AnnoMetadata(Base):
     __tablename__ = 'annotation_table_metadata'
     id = Column(Integer, primary_key=True)
     schema_type = Column(String(100), nullable=False)
@@ -14,3 +14,15 @@ class Metadata(Base):
     reference_table = Column(String(100), nullable=True)
     flat_segmentation_source = Column(String(300), nullable=True)
 
+
+class SegmentationMetadata(Base):
+    __tablename__ = 'segmentation_table_metadata'
+    id = Column(Integer, primary_key=True)
+    schema_type = Column(String(100), nullable=False)
+    table_id = Column(String(100), nullable=False, unique=True)
+    valid = Column(Boolean)
+    created = Column(DateTime, nullable=False)
+    deleted = Column(DateTime, nullable=True)
+    segmentation_source = Column(String(255), nullable=True)
+    pcg_table_name = Column(String(255), nullable=False)
+    annotation_table = Column(String(100), ForeignKey('annotation_table_metadata.table_id'))
