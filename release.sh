696522ef37a5935113fd0d1b435f42bdf7e404e7
Derrick <dlbrittain@gmail.com>
2021-02-23 11:45:30 -0800

    fixed path

--- release.sh
+++ release.sh
@@ -3,4 +3,4 @@ bumpversion $1
 git push && git push --tags
 TAG=$(git describe --tags)
 python setup.py sdist
-twine upload dist/dynamicannotationdb-${TAG:1}.tar.gz
+twine upload dist/DynamicAnnotationDB-${TAG:1}.tar.gz
