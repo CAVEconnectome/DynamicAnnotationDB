218afea2218250e37432ed045c785fd2c073d83c
Derrick <dlbrittain@gmail.com>
2021-04-07 14:30:11 -0700

    test: update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,98 +1,125 @@
-from os import environ, getenv
-import uuid
-import warnings
-import pytest
-import docker
-import psycopg2
 import logging
 import time
-from typing import Any, Callable, Type
-from functools import wraps
+import uuid
+import warnings
+
+import docker
+import psycopg2
+import pytest
+from dynamicannotationdb.annotation_client import DynamicAnnotationClient
 from dynamicannotationdb.interface import DynamicAnnotationInterface
 from dynamicannotationdb.materialization_client import DynamicMaterializationClient
-from dynamicannotationdb.annotation_client import DynamicAnnotationClient
 
 logging.basicConfig(level=logging.DEBUG)
 test_logger = logging.getLogger()
 
-POSTGIS_DOCKER_IMAGE = "mdillon/postgis:latest"
-ALIGNED_VOLUME = 'test_volume'
-DB_HOST = '127.0.0.1'
-TABLE_NAME = 'anno_test'
-SCHEMA_TYPE = 'synapse'
-PCG_TABLE_NAME = 'test_pcg'
-SQL_URI = f"postgres://postgres:postgres@{DB_HOST}:5432/{ALIGNED_VOLUME}"
 
-
-
-db_enviroment = [
-    f"POSTGRES_USER=postgres",
-    f"POSTGRES_PASSWORD=postgres",
-    f"POSTGRES_DB={ALIGNED_VOLUME}"
-]
-
-db_ports = {"5432/tcp": 5432}
-
-USE_LOCAL_DB = getenv("USE_LOCAL_TEST_DB", False)
+def pytest_addoption(parser):
+    parser.addoption(
+        "--docker",
+        action="store",
+        default=False,
+        help="Use docker for postgres testing",
+    )
 
 
 @pytest.fixture(scope="session")
-def docker_client() -> docker.DockerClient:
-    yield docker.from_env()
+def docker_mode(request):
+    return request.config.getoption("--docker")
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "docker: use postgres in docker")
 
 
 @pytest.fixture(scope="session")
-def annotation_client():
-    annotation_client = DynamicAnnotationClient(ALIGNED_VOLUME, SQL_URI)
-    return annotation_client
-
-@pytest.fixture(scope="session")
-def dynamic_annotation_interface():
-    dynamic_annotation_interface = DynamicAnnotationInterface(SQL_URI)
-    return dynamic_annotation_interface
+def database_metadata() -> dict:
+    yield {
+        "postgis_docker_image": "postgis/postgis:9.6-2.5",
+        "db_host": "localhost",
+        "sql_uri": f"postgresql://postgres:postgres@localhost:5432/test_volume",
+    }
 
 
 @pytest.fixture(scope="session")
-def materialization_client():
-    materialization_client = DynamicMaterializationClient(ALIGNED_VOLUME, SQL_URI)
-    return materialization_client
+def annotation_metadata():
+    yield {
+        "aligned_volume": "test_volume",
+        "table_name": "anno_test",
+        "schema_type": "synapse",
+        "pcg_table_name": "test_pcg",
+    }
+
 
 @pytest.fixture(scope="session", autouse=True)
-def postgis_server(docker_client: docker.DockerClient) -> None:
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
+def postgis_server(docker_mode, database_metadata: dict) -> None:
 
-    if not USE_LOCAL_DB:
-        test_logger.info(f"PULLING {POSTGIS_DOCKER_IMAGE} IMAGE")
+    postgis_docker_image = database_metadata["postgis_docker_image"]
+    sql_uri = database_metadata["sql_uri"]
+
+    if docker_mode:
+        test_logger.info(f"PULLING {postgis_docker_image} IMAGE")
         try:
-            docker_client.images.pull(repository=POSTGIS_DOCKER_IMAGE)
+            docker_client = docker.from_env()
+            docker_client.images.pull(repository=postgis_docker_image)
         except Exception:
             test_logger.exception("Failed to pull postgres image")
 
         container_name = f"test_postgis_server_{uuid.uuid4()}"
 
         test_container = docker_client.containers.run(
-            image=POSTGIS_DOCKER_IMAGE,
+            image=postgis_docker_image,
             detach=True,
-            hostname='test_postgres',
+            hostname="test_postgres",
             auto_remove=True,
             name=container_name,
-            environment=db_enviroment,
-            ports=db_ports,
+            environment=[
+                "POSTGRES_USER=postgres",
+                "POSTGRES_PASSWORD=postgres",
+                "POSTGRES_DB=test_volume",
+            ],
+            ports={"5432/tcp": 5432},
         )
 
-
-
-        test_logger.info('STARTING IMAGE')
+        test_logger.info("STARTING IMAGE")
         try:
             time.sleep(10)
-            check_database(SQL_URI)
-            yield test_container
-        finally:
-            container = docker_client.containers.get(container_name)
-            container.stop()
-    else:
-        yield
-        return
+            check_database(sql_uri)
+        except Exception as e:
+            raise (e)
+    yield test_container
+    if docker_mode:
+        warnings.filterwarnings(
+            action="ignore", message="unclosed", category=ResourceWarning
+        )
+        container = docker_client.containers.get(container_name)
+        container.stop()
+
+
+@pytest.fixture(scope="session")
+def dynamic_annotation_interface(postgis_server, database_metadata):
+    sql_uri = database_metadata["sql_uri"]
+    interface = DynamicAnnotationInterface(sql_uri)
+    yield interface
+    interface.cached_session.close()
+
+
+@pytest.fixture(scope="session")
+def annotation_client(postgis_server, annotation_metadata, database_metadata):
+    aligned_volume = annotation_metadata["aligned_volume"]
+    sql_uri = database_metadata["sql_uri"]
+    anno_client = DynamicAnnotationClient(aligned_volume, sql_uri)
+    yield anno_client
+    anno_client.cached_session.close()
+
+
+@pytest.fixture(scope="session")
+def materialization_client(postgis_server, annotation_metadata, database_metadata):
+    aligned_volume = annotation_metadata["aligned_volume"]
+    sql_uri = database_metadata["sql_uri"]
+    mat_client = DynamicMaterializationClient(aligned_volume, sql_uri)
+    yield mat_client
+    mat_client.cached_session.close()
 
 
 def check_database(sql_uri: str) -> None:  # pragma: no cover
