c1acb4e1151b5d4eeca2385d92e1072fa01d9d47
Derrick <dlbrittain@gmail.com>
2021-10-20 18:57:40 -0700

    fix: formatting

--- dynamicannotationdb/key_utils.py
+++ dynamicannotationdb/key_utils.py
@@ -1,5 +1,5 @@
 def build_table_id(aligned_volume: str, table_name: str) -> str:
-    """Combines aligned_volume name and specified table name to 
+    """Combines aligned_volume name and specified table name to
     create specific table id
 
 
@@ -13,13 +13,15 @@ def build_table_id(aligned_volume: str, table_name: str) -> str:
     Returns
     -------
     str
-        formatted table_id 
+        formatted table_id
     """
     return f"{aligned_volume}__{table_name}"
 
-def build_segmentation_table_name(annotation_table_name: str, 
-                                segmentation_source: str) -> str:
-    """Creates a table name that combines annotation table and appends 
+
+def build_segmentation_table_name(
+    annotation_table_name: str, segmentation_source: str
+) -> str:
+    """Creates a table name that combines annotation table and appends
     segmentation table name
 
     Parameters
@@ -32,10 +34,11 @@ def build_segmentation_table_name(annotation_table_name: str,
     Returns
     -------
     str
-        formatted name of table combining the annotation table id with 
+        formatted name of table combining the annotation table id with
         chunkedgraph segmentation source name
     """
     return f"{annotation_table_name}__{segmentation_source}"
+
 
 def get_table_name_from_table_id(table_id: str) -> str:
     """Extracts table name from table_id string
