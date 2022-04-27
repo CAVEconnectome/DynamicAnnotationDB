abe2f7bca00118213c76ff673daf072b317b7271
Derrick Brittain <dlbrittain@gmail.com>
2022-04-27 14:36:34 -0700

    feat: move materialziation engine models into dadb

--- dynamicannotationdb/models.py
+++ dynamicannotationdb/models.py
@@ -9,7 +9,46 @@ from sqlalchemy import (
     Integer,
     String,
     Text,
+    Enum,
 )
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+MatBase = declarative_base()
+
+
+class AnalysisVersion(Base):
+    __tablename__ = "analysisversion"
+    id = Column(Integer, primary_key=True)
+    datastack = Column(String(100), nullable=False)
+    version = Column(Integer, nullable=False)
+    time_stamp = Column(DateTime, nullable=False)
+    valid = Column(Boolean)
+    expires_on = Column(DateTime, nullable=True)
+
+    def __repr__(self):
+        return f"{self.datastack}__mat{self.version}"
+
+
+class AnalysisTable(Base):
+    __tablename__ = "analysistables"
+    id = Column(Integer, primary_key=True)
+    aligned_volume = Column(String(100), nullable=False)
+    schema = Column(String(100), nullable=False)
+    table_name = Column(String(100), nullable=False)
+    valid = Column(Boolean)
+    created = Column(DateTime, nullable=False)
+    analysisversion_id = Column(Integer, ForeignKey("analysisversion.id"))
+    analysisversion = relationship("AnalysisVersion")
+
+
+class MaterializedMetadata(MatBase):
+    __tablename__ = "materializedmetadata"
+    id = Column(Integer, primary_key=True)
+    schema = Column(String(100), nullable=False)
+    table_name = Column(String(100), nullable=False)
+    row_count = Column(Integer, nullable=False)
+    materialized_timestamp = Column(DateTime, nullable=False)
 
 
 class AnnoMetadata(Base):
