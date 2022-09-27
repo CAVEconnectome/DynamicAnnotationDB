58e25657f0450702b49370eef9b2f28b8affa45d
Derrick Brittain <dlbrittain@gmail.com>
2022-09-27 15:00:23 -0700

    Bump version: 5.1.3 → 5.1.4

--- dynamicannotationdb/migration/__init__.py
+++ dynamicannotationdb/migration/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "5.1.3"
+__version__ = "5.1.4"
 
 from dynamicannotationdb.migration.migrate import DynamicMigration
 from dynamicannotationdb.migration.alembic.run import run_alembic_migration
