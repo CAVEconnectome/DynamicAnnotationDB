09979b0c02b4b79c5d73732fb6bd0ce05e7a5f53
Forrest Collman <forrest.collman@gmail.com>
2022-10-27 15:16:06 -0700

    Bump version: 5.3.2 → 5.3.3

--- dynamicannotationdb/migration/__init__.py
+++ dynamicannotationdb/migration/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "5.3.2"
+__version__ = "5.3.3"
 
 from dynamicannotationdb.migration.migrate import DynamicMigration
 from dynamicannotationdb.migration.alembic.run import run_alembic_migration
